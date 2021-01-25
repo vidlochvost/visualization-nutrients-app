13b1e6b5e573ba4fa77c8cc153badc9a343b233b
Miroslav Mikolaj <469508@mail.muni.cz>
2021-01-25 21:41:38 +0100

    EDIT minor edits and change of a filename

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 8080;
 
 app.use(express.static('public'))
 app.use('/public', express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => res.sendFile(__dirname + "/index.html"))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log('Our app is running on http://localhost:' + port))
